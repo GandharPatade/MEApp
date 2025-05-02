29a06ae6bca37c78db88845b3e910333efc04491
AniketMGawade95 <aniketmgawade2019@gmail.com>
2025-05-02 13:50:14 +0530

    fg

--- MasterPages/AdminMasterPage.Master.designer.cs
+++ MasterPages/AdminMasterPage.Master.designer.cs
@@ -150,15 +150,6 @@ namespace MEApp.MasterPages
         protected global::System.Web.UI.WebControls.HyperLink HyperLink12;
 
         /// <summary>
-        /// HyperLink13 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink13;
-
-        /// <summary>
         /// HyperLink17 control.
         /// </summary>
         /// <remarks>
